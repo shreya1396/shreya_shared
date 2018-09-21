6f77a0586c7d3ae179140028af5151b95f272908
shreya1396 <42531964+shreya1396@users.noreply.github.com>
2018-09-21 10:08:47 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,1 +1,25 @@
+def call(body) {
 
+        def config = [:]
+        body.resolveStrategy = Closure.DELEGATE_FIRST
+        body.delegate = config
+        body()
+
+        node {
+            // Clean workspace before doing anything
+            deleteDir()
+
+            try {
+                stage ('Clone') {
+                    checkout scm
+                }
+                stage ('Build') {
+                    sh "echo 'building ${config.projectName} ...'"
+                }
+                
+            } catch (err) {
+                currentBuild.result = 'FAILED'
+                throw err
+            }
+        }
+    }
